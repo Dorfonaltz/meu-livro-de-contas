8f2b3e9255f755116a45721eeb538bfbd37d831b
Vonsoski <rodolfovonsoski@gmail.com>
2026-08-11 02:59:13 -0300

    Adiciona sw.js para instalação do FINTRIQ

---
+++ sw.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = "fintriq-shell-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.svg",
+  "./icons/icon-512.svg",
+  "./icons/icon-maskable.svg",
+].map((path) => new URL(path, self.location.href).href);
+const INDEX_URL = new URL("./index.html", self.location.href).href;
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)),
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key.startsWith("fintriq-") && key !== CACHE_NAME)
+            .map((key) => caches.delete(key)),
+        ),
+      )
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+  if (request.method !== "GET") return;
+  const url = new URL(request.url);
+
+  if (request.mode === "navigate") {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(INDEX_URL, copy));
+          return response;
+        })
+        .catch(() => caches.match(INDEX_URL)),
+    );
+    return;
+  }
+
+  if (url.origin === self.location.origin) {
+    event.respondWith(
+      caches.match(request).then(
+        (cached) =>
+          cached ||
+          fetch(request).then((response) => {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+            return response;
+          }),
+      ),
+    );
+  }
+});
